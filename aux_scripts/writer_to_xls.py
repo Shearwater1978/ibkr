15d3d59f0ec5bafc4a3a8800bb9bc0addd7f89b6
Uglykoyote <uglykoyote@yandex.ru>
2023-02-19 16:41:11 +0100

    Adding new worksheet (#15)
    * Added new Excell page with summary
    
    * Small fix

--- aux_scripts/writer_to_xls.py
+++ aux_scripts/writer_to_xls.py
@@ -30,7 +30,7 @@ def writeWorkSheet(file_path: str, items: list, worksheetname: str, headers: dic
 
 
 def unionDivsStocksXls():
-    headers = [ 'Тип актива', 'Сумма в валюте', 'Сумма в злотых' ]
+    headers = [ 'Asset type', 'Amount in currency', 'Amount in pln' ]
     
     mainPandas = pd.DataFrame(columns = headers)
     stocksPandas = pd.DataFrame()
@@ -60,6 +60,22 @@ def unionDivsStocksXls():
     mainPandas.at[4, 'Тип актива'] = 'Дивиденды. Комиссия'
     mainPandas.at[4, 'Сумма в валюте'] = round(divsTaxPandas['DivTaxInCurrency'].sum(), 3)
     mainPandas.at[4, 'Сумма в злотых'] = round(divsTaxPandas['DivTaxInPln'].sum(), 3)
+    
+    mainPandas.at[1, 'Asset type'] = 'Stocks. Profit/Lose'
+    mainPandas.at[1, 'Amount in currency'] = round(stocksPandas['ProfitInCurrency'].sum(), 3)
+    mainPandas.at[1, 'Amount in pln'] = round(stocksPandas['ProfitInPln'].sum(), 3)
+    
+    mainPandas.at[2, 'Asset type'] = 'Stocks. Fees'
+    mainPandas.at[2, 'Amount in currency'] = round(stocksPandas['TaxInCurrency'].sum(), 3)
+    mainPandas.at[2, 'Amount in pln'] = round(stocksPandas['TaxInPln'].sum(), 3)
+    
+    mainPandas.at[3, 'Asset type'] = 'Dividends. Profit'
+    mainPandas.at[3, 'Amount in currency'] = round(divsIncomePandas['DivInCurrency'].sum(), 3)
+    mainPandas.at[3, 'Amount in pln'] = round(divsIncomePandas['DivInPln'].sum(), 3)
+    
+    mainPandas.at[4, 'Asset type'] = 'Dividends. Fees'
+    mainPandas.at[4, 'Amount in currency'] = round(divsTaxPandas['DivTaxInCurrency'].sum(), 3)
+    mainPandas.at[4, 'Amount in pln'] = round(divsTaxPandas['DivTaxInPln'].sum(), 3)
     
     with pd.ExcelWriter('ibkr_report_joint.xlsx') as writer:
         mainPandas.to_excel(writer, sheet_name='main', index=False)
