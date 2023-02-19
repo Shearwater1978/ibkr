93350271740d733c10031af71e7e6637fdb579f2
Uglykoyote <uglykoyote@yandex.ru>
2023-02-19 09:18:41 +0100

    Added new module

--- aux_scripts/writer_to_xls.py
+++ aux_scripts/writer_to_xls.py
@@ -30,22 +30,26 @@ def writeWorkSheet(file_path: str, items: list, worksheetname: str, headers: dic
 
 
 def unionDivsStocksXls():
-    divsPandas = pd.DataFrame()
     stocksPandas = pd.DataFrame()
-    divsPandas = pd.read_excel('ibkr_report_divs.xls',sheet_name=0)
+    divsIncomePandas = pd.DataFrame()
     stocksPandas = pd.read_excel('ibkr_report_stocks.xls',sheet_name=0)
+    divsIncomePandas = pd.read_excel('ibkr_report_div_income.xls',sheet_name=0)
+    divsTaxPandas = pd.read_excel('ibkr_report_div_tax.xls',sheet_name=0)    
     
     wb = xlsxwriter.Workbook()
     ws1 = wb.add_worksheet('stocks')
-    ws2 = wb.add_worksheet('divs')
+    ws2 = wb.add_worksheet('divsicome')
+    ws3 = wb.add_worksheet('divstax')
     
     with pd.ExcelWriter('ibkr_report_joint.xlsx') as writer:
-        divsPandas.to_excel(writer, sheet_name='divs', header=False, index=False)
         stocksPandas.to_excel(writer, sheet_name='stocks', header=False, index=False)
+        divsIncomePandas.to_excel(writer, sheet_name='divsincome', header=False, index=False)
+        divsTaxPandas.to_excel(writer, sheet_name='divtax', header=False, index=False)
     
     # Remove single xls file
-    os.remove('ibkr_report_divs.xls')
     os.remove('ibkr_report_stocks.xls')
+    os.remove('ibkr_report_div_income.xls')
+    os.remove('ibkr_report_div_tax.xls')
 
             
 def main():
