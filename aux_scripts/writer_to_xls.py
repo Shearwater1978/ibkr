3866359797d5082840424769f319269aaee15dcf
Alex <uglykoyote@yandex.ru>
2024-08-01 10:53:03 +0200

    Fix

--- aux_scripts/writer_to_xls.py
+++ aux_scripts/writer_to_xls.py
@@ -30,13 +30,14 @@ def writeWorkSheet(file_path: str, items: list, worksheetname: str, headers: dic
     workbook.close()
 
 
-def unionDivsStocksXls():
+def unionDivsStocksXls(ibkr_report_stocks_is_exist):
     headers = ['Asset type', 'Amount in currency', 'Amount in pln']
 
     mainPandas = pd.DataFrame(columns = headers)
     stocksPandas = pd.DataFrame()
     divsIncomePandas = pd.DataFrame()
-    stocksPandas = pd.read_excel('ibkr_report_stocks.xls', sheet_name=0, index_col=0, header=1)
+    if ibkr_report_stocks_is_exist:
+        stocksPandas = pd.read_excel('ibkr_report_stocks.xls', sheet_name=0, index_col=0, header=1)
     divsIncomePandas = pd.read_excel('ibkr_report_div_income.xls', sheet_name=0, index_col=0, header=1)
     divsTaxPandas = pd.read_excel('ibkr_report_div_tax.xls', sheet_name=0, index_col=0, header=1)
 
@@ -46,13 +47,14 @@ def unionDivsStocksXls():
     ws2 = wb.add_worksheet('divsicome')
     ws3 = wb.add_worksheet('divstax')
 
-    mainPandas.at[1, 'Asset type'] = 'Stocks IB. Profit/Lose'
-    mainPandas.at[1, 'Amount in currency'] = round(stocksPandas['ProfitInCurrency'].sum(), 3)
-    mainPandas.at[1, 'Amount in pln'] = round(stocksPandas['ProfitInPln'].sum(), 3)
+    if ibkr_report_stocks_is_exist:
+        mainPandas.at[1, 'Asset type'] = 'Stocks IB. Profit/Lose'
+        mainPandas.at[1, 'Amount in currency'] = round(stocksPandas['ProfitInCurrency'].sum(), 3)
+        mainPandas.at[1, 'Amount in pln'] = round(stocksPandas['ProfitInPln'].sum(), 3)
 
-    mainPandas.at[2, 'Asset type'] = 'Stocks IB. Fees'
-    mainPandas.at[2, 'Amount in currency'] = round(stocksPandas['TaxInCurrency'].sum(), 3)
-    mainPandas.at[2, 'Amount in pln'] = round(stocksPandas['TaxInPln'].sum(), 3)
+        mainPandas.at[2, 'Asset type'] = 'Stocks IB. Fees'
+        mainPandas.at[2, 'Amount in currency'] = round(stocksPandas['TaxInCurrency'].sum(), 3)
+        mainPandas.at[2, 'Amount in pln'] = round(stocksPandas['TaxInPln'].sum(), 3)
 
     mainPandas.at[3, 'Asset type'] = 'Dividends IB. Profit'
     mainPandas.at[3, 'Amount in currency'] = round(divsIncomePandas['DivInCurrency'].sum(), 3)
@@ -64,12 +66,14 @@ def unionDivsStocksXls():
 
     with pd.ExcelWriter('ibkr_report_joint.xlsx') as writer:
         mainPandas.to_excel(writer, sheet_name='main', index=False)
-        stocksPandas.to_excel(writer, sheet_name='stocks', index=False)
+        if ibkr_report_stocks_is_exist:
+            stocksPandas.to_excel(writer, sheet_name='stocks', index=False)
         divsIncomePandas.to_excel(writer, sheet_name='divsincome', index=False)
         divsTaxPandas.to_excel(writer, sheet_name='divtax', index=False)
 
     # Remove single xls file
-    os.remove('ibkr_report_stocks.xls')
+    if ibkr_report_stocks_is_exist:
+        os.remove('ibkr_report_stocks.xls')
     os.remove('ibkr_report_div_income.xls')
     os.remove('ibkr_report_div_tax.xls')
 
